bc23e16b6eda0fb76094384350b22679785d5b6b
angeliferreira <angeliferreira@b340a035-0ee8-41d8-ab7e-97649ed12cc3>
2011-11-26 00:48:10 +0000

    * Methods getElement(), move() and updatePositions() in class Grid;
    26 Tests still green :-)

--- trunk/Sokobao2000/src/main/Grid.java
+++ trunk/Sokobao2000/src/main/Grid.java
@@ -3,8 +3,6 @@ package main;
 import java.awt.Point;
 
 import main.directions.Direction;
-import main.directions.East;
-import main.gameElement.GameElement;
 
 public class Grid {
 
@@ -30,7 +28,7 @@ public class Grid {
 		this.matrix[position.y][position.x] = character;
 	}
 	
-	public Point getHeroInitialPosition() {
+	public Point getHeroPosition() {
 		for (int i = 0; i < this.matrix.length; i++) {
 			for (int j = 0; j < this.matrix.length; j++) {
 				if (this.matrix[i][j] == 'H') return new Point(j, i);
@@ -43,21 +41,30 @@ public class Grid {
 		return matrix;
 	}
 	
-//	public GameElement getElement(Point position) {
-//		return this.matrix[position.y][position.x];
-//	}
-//	
-//	public void move(GameElement element, Direction direction) {
-//		Point desiredPosition = direction.newPosition(element.getPosition()); 
-//		if (isDesiredPositionAWall(desiredPosition)) return;
-//		if (isDesiredPositionABlock(desiredPosition)) {
-//			Point blockDesiredPosition = direction.newPosition(this.getElement(desiredPosition).getPosition()); 
-//			if (isDesiredPositionABlockOrWall(blockDesiredPosition)) return;
-//			this.setPositionWithValue(blockDesiredPosition, 'B');
-//		}
-//		this.setPositionWithValue(this.heroPosition, ' ');
-//		this.setPositionWithValue(desiredPosition, 'H');
-//		this.heroPosition = desiredPosition;
-//	}
+//	TODO -> CHANGE RETURN TO GameElement
+	public Character getElement(Point position) {
+		return this.matrix[position.y][position.x];
+	}
 	
+	public void move(Point position, Direction direction) {
+		Point desiredPosition = direction.newPosition(position); 
+		if (isDesiredPositionAWall(desiredPosition)) return;
+		if (isDesiredPositionABlock(desiredPosition)) {
+			Point blockDesiredPosition = direction.newPosition(desiredPosition); 
+			if (isDesiredPositionABlockOrWall(blockDesiredPosition)) return;
+			this.updatePositions(desiredPosition, blockDesiredPosition);
+		}
+		this.updatePositions(position, desiredPosition);
+	}
+	
+	public void updatePositions(Point oldPosition, Point newPosition) {
+//		GameElement element = this.getElement(oldPosition);
+		Character element = this.getElement(oldPosition);
+		
+//		this.matrix[oldPosition.y][oldPosition.x] = new DumbElement();
+		this.matrix[oldPosition.y][oldPosition.x] = ' ';
+		
+		this.matrix[newPosition.y][newPosition.x] = element;
+	}
+
 }
--- trunk/Sokobao2000/src/main/Sokobao2000.java
+++ trunk/Sokobao2000/src/main/Sokobao2000.java
@@ -3,109 +3,57 @@ package main;
 import java.awt.Point;
 
 import main.directions.East;
+import main.directions.North;
+import main.directions.South;
+import main.directions.West;
 import main.gameElement.GameElement;
+import main.gameElement.Hero;
 
 public class Sokobao2000 {
 
 	private Grid grid;
-	private Point heroPosition;
 	private GameElement hero;
 		
 	public Sokobao2000() {
 		this.grid = LevelGame.gridLevel1();
-		this.heroPosition = this.grid.getHeroInitialPosition();
+		this.hero = new Hero(this.grid.getHeroPosition());
 	}
 
 	public void moveHeroEast() {
-		int j = this.heroPosition.x;
-		int i = this.heroPosition.y;
-		Point heroDesiredPosition = new Point(j+1, i);
-		if (isDesiredPositionAWall(heroDesiredPosition)) return;
-		if (isDesiredPositionABlock(heroDesiredPosition)) {
-			Point blockDesiredPosition = new Point(j+2, i);
-			if (isDesiredPositionABlockOrWall(blockDesiredPosition)) return;
-			this.grid.setPositionWithValue(blockDesiredPosition, 'B');
-		}
-		this.grid.setPositionWithValue(this.heroPosition, ' ');
-		this.grid.setPositionWithValue(heroDesiredPosition, 'H');
-		this.heroPosition = heroDesiredPosition;
-//		this.grid.move(this.hero, East.getInstance());
+		this.grid.move(this.hero.getPosition(), East.getInstance());
 	}
 
 	public void moveHeroWest() {
-		int j = this.heroPosition.x;
-		int i = this.heroPosition.y;
-		Point heroDesiredPosition = new Point(j-1, i);
-		if (isDesiredPositionAWall(heroDesiredPosition)) return;
-		if (isDesiredPositionABlock(heroDesiredPosition)) {
-			Point blockDesiredPosition = new Point(j-2, i);
-			if (isDesiredPositionABlockOrWall(blockDesiredPosition)) return;
-			this.grid.setPositionWithValue(blockDesiredPosition, 'B');
-		}
-		this.grid.setPositionWithValue(this.heroPosition, ' ');
-		this.grid.setPositionWithValue(heroDesiredPosition, 'H');
-		this.heroPosition = heroDesiredPosition;
+		this.grid.move(this.hero.getPosition(), West.getInstance());
 	}
 	
 	public void moveHeroNorth() {
-		int j = this.heroPosition.x;
-		int i = this.heroPosition.y;
-		Point heroDesiredPosition = new Point(j, i-1);
-		if (isDesiredPositionAWall(heroDesiredPosition)) return;
-		if (isDesiredPositionABlock(heroDesiredPosition)) {
-			Point blockDesiredPosition = new Point(j, i-2);
-			if (isDesiredPositionABlockOrWall(blockDesiredPosition)) return;
-			this.grid.setPositionWithValue(blockDesiredPosition, 'B');
-		}
-		this.grid.setPositionWithValue(this.heroPosition, ' ');
-		this.grid.setPositionWithValue(heroDesiredPosition, 'H');
-		this.heroPosition = heroDesiredPosition;
+		this.grid.move(this.hero.getPosition(), North.getInstance());
 	}
 
 	public void moveHeroSouth() {
-		int j = this.heroPosition.x;
-		int i = this.heroPosition.y;
-		Point heroDesiredPosition = new Point(j, i+1);
-		if (isDesiredPositionAWall(heroDesiredPosition)) return;
-		if (isDesiredPositionABlock(heroDesiredPosition)) {
-			Point blockDesiredPosition = new Point(j, i+2);
-			if (isDesiredPositionABlockOrWall(blockDesiredPosition)) return;
-			this.grid.setPositionWithValue(blockDesiredPosition, 'B');
-		}
-		this.grid.setPositionWithValue(this.heroPosition, ' ');
-		this.grid.setPositionWithValue(heroDesiredPosition, 'H');
-		this.heroPosition = heroDesiredPosition;
+		this.grid.move(this.hero.getPosition(), South.getInstance());
 	}
 	
-	private boolean isDesiredPositionAWall(Point desiredPosition) {
-		return this.grid.isDesiredPositionAWall(desiredPosition);
-	}
-
-	private boolean isDesiredPositionABlockOrWall(Point desiredPosition) {
-		return this.grid.isDesiredPositionABlockOrWall(desiredPosition);
-	}
-
-	private boolean isDesiredPositionABlock(Point desiredPosition) {
-		return this.grid.isDesiredPositionABlockOrWall(desiredPosition);
-	}
-
 	public void setGrid(Grid grid) {
 		this.grid = grid;
-		this.heroPosition = this.grid.getHeroInitialPosition();
 	}
 
 	public Grid getGrid() {
 		return this.grid;
 	}
 
-	public Point getHeroPosition() {
-		return this.heroPosition;
-	}
-	
 	public void setHeroPosition(Point heroNewPosition) {
-		this.grid.setPositionWithValue(heroPosition, ' ');
-		this.grid.setPositionWithValue(heroNewPosition, 'H');
-		this.heroPosition = heroNewPosition;
+		this.grid.updatePositions(this.hero.getPosition(), heroNewPosition);
+		this.hero.setPosition(heroNewPosition);
+	}
+
+	public void setHero(GameElement hero) {
+		this.hero = hero;
+	}
+
+	public GameElement getHero() {
+		return hero;
 	}
 
 }
